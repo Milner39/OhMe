a7111c04a15fd81f50eda4864ff2091edcc06fb2
HeyItsFinnVR <finn1011@live.com>
2024-03-14 20:21:14 +0000

    basic changes to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite"
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+/** @type {import("vite").UserConfig} */
+const config = {
+	plugins: [
+		sveltekit()
+	],
+	server: {
+		// Allows devices on other networks to access the site
+		host: true,
+	}
+}
+export default config
