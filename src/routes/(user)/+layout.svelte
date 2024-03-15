51f2f92c3565e594e791772c98f5e79428468bdc
HeyItsFinnVR <finn1011@live.com>
2024-03-15 23:12:50 +0000

    Basic user page layout

---
+++ src/routes/(user)/+layout.svelte
@@ -0,0 +1,46 @@
+<script>
+    import "$lib/styles/global.scss";
+
+    import Header from "$lib/components/Header/Header.svelte"
+</script>
+
+<Header let:Nav let:Notice>
+    <Nav links={[
+        {
+            text: "Friends",
+            href: "/friends"
+        },
+        {
+            text: "Lorem",
+            href: "/"
+        },
+        {
+            text: "Lorem",
+            href: "/"
+        },
+        {
+            text: "Lorem",
+            href: "/"
+        },
+        {
+            text: "Lorem",
+            href: "/"
+        },
+        {
+            text: "Settings",
+            href: "/settings"
+        },
+    ]}>
+        <svelte:fragment slot="brand">
+            <h1>OhMe</h1>
+        </svelte:fragment>
+        <button>Login</button>
+        <svelte:fragment slot="static">
+            <button>Join</button>
+        </svelte:fragment>
+    </Nav>
+    <Notice>
+        <h1>Alert Here</h1>
+    </Notice>
+</Header>
+<slot/>
