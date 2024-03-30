1c27d310cffe1ff5d4987f9deb20cb1b17b16ffe
HeyItsFinnVR <finn1011@live.com>
2024-03-30 23:53:36 +0000

    re-enabled login page redirect and fixed spelling mistakes

--- src/routes/(main)/(private)/+layout.server.js
+++ src/routes/(main)/(private)/+layout.server.js
@@ -1,7 +1,7 @@
 // Import function to redirect user
 import { redirect } from '@sveltejs/kit'
 
-// Define "loginPage" so user isn't from the login page to the login page
+// Define "loginPage" so user isn't redirected from the login page back to the login page
 // Prevents infinite loop which causes error
 const loginPage = "/login"
 
@@ -10,7 +10,7 @@ export const load = async (event) => {
     // Don't redirect if already at redirect target
     if (event.url.pathname === loginPage) { return }
 
-    // If no user has no session, redirect to login page
+    // If user has no session, redirect to login page
     // if (!event.locals.session) {
     //     redirect(302, loginPage)
     // }
