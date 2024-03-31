0654baf451530eb3a6c78e537548c26f2fb6c5e9
HeyItsFinnVR <finn1011@live.com>
2024-03-31 11:02:45 +0100

    changed route structure

--- src/routes/(main)/(private)/+layout.server.js
+++ src/routes/(main)/(private)/+layout.server.js
@@ -1,15 +1,8 @@
 // Import function to redirect user
 import { redirect } from '@sveltejs/kit'
 
-// Define "loginPage" so user isn't redirected from the login page back to the login page
-// Prevents infinite loop which causes error
-const loginPage = "/login"
-
 // Define load function to be ran on every page down this route
 export const load = async (event) => {
-    // Don't redirect if already at redirect target
-    if (event.url.pathname === loginPage) { return }
-
     // If user has no session, redirect to login page
     if (!event.locals.session) {
         redirect(302, loginPage)
