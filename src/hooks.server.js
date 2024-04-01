1c13db9c1dfe3ae884891253320db37f46db18d5
HeyItsFinnVR <finn1011@live.com>
2024-04-01 13:25:39 +0100

    added session extention

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,9 @@
 // Import function used to run multiple hooks in sequence
 import { sequence } from "@sveltejs/kit/hooks"
 
+// Import prisma client
+import { client as prismaClient } from "$lib/server/prisma"
+
 // Import lucia client
 import { client as luciaClient } from "$lib/server/lucia"
 
@@ -28,12 +31,33 @@ const luciaHandle = async ({ event, resolve }) => {
         return resolve(event)
     }
 
-    // If session exists
+    // Get date 7 days from now
+    const refreshDate = new Date()
+    refreshDate.setDate(refreshDate.getDate() +7)
+    // If session expires in less than 7 days
+    if (session.expiresAt < refreshDate) {
+        // Extend expiry date
+        // Create new expiry date 21 days from now
+        const expiryDate = new Date()
+        expiryDate.setDate(expiryDate.getDate() +21)
+        // Make changes to session in the database
+        try {
+            await prismaClient.Session.update({
+                where: {
+                    id: sessionId
+                },
+                data: {
+                    expiresAt: expiryDate
+                }
+            })
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
     event.locals.user = user
     event.locals.session = session
     return resolve(event)
-
-    // TODO: Extending session expirery
 }
 
 // Export handle to be run
