1f7b5b1d915aa60614238edbda0f460910c49803
HeyItsFinnVR <finn1011@live.com>
2024-04-03 17:09:33 +0100

    Created settings page

---
+++ src/routes/(main)/(private)/(user)/settings/+page.server.js
@@ -0,0 +1,273 @@
+// Import sanitizer to ensure all user inputs are valid
+import { sanitizer } from "$lib/server/sanitize.js"
+// Import the prisma client to interact with database
+import { client as prismaClient } from "$lib/server/prisma"
+// Import a hashing function to hash & unhash strings
+import { stringHasher } from "$lib/server/argon"
+
+// Define function to check if errors have been caught
+const formHasErrors = (obj) => {
+    if (Object.keys(obj).length > 0) {
+        return true
+    }
+}
+
+// Define actions
+export const actions = {
+    username: async ({ request, locals }) => {
+        // Variable to hold error information
+        let errors = {}
+        
+        // Get user from locals
+        const { user } = locals
+
+        // If no user
+        if (!user) {
+            return {
+                status: 403,
+                errors: {server: "User not signed in"}
+            }
+        }
+
+        // Get form data
+        const formData = Object.fromEntries(await request.formData())
+        
+        // Sanitize client input
+        if (!sanitizer.username(formData.username)) {
+            errors.username = "Invalid username"
+        }
+
+        // Return if inputs not valid
+        if (formHasErrors(errors)) {
+            return {
+                status: 422,
+                errors
+            }
+        }
+
+        // Update User entry in db
+        try {
+            await prismaClient.User.update({
+                // Set filter feilds
+                where: {
+                    id:  user.id
+                },
+                // Set update feilds
+                data: {
+                    username: formData.username
+                }
+            })
+        } catch (err) {
+            // Catch error, match error code to
+            // appropriate error message
+            switch (err.code) {
+                case "P2002":
+                    errors.username = "Username taken"
+                default:
+                    errors.server = "Unable to change information"
+            }
+        }
+
+        // Return if entry cannot be updated
+        if (formHasErrors(errors)) {
+            return {
+                status: 422,
+                errors
+            }
+        }
+
+        // Return if no errors
+        return {
+            status: 200,
+            errors
+        }
+    },
+
+    email: async ({ request, locals }) => {
+        // Variable to hold error information
+        let errors = {}
+
+        // Get user from locals
+        const { user } = locals
+
+        // If no user
+        if (!user) {
+            return {
+                status: 403,
+                errors: {server: "User not signed in"}
+            }
+        }
+
+        // Get form data
+        const formData = Object.fromEntries(await request.formData())
+        
+        // Sanitize client input
+        if (!sanitizer.email(formData.email)) {
+            errors.email = "Invalid email"
+        }
+
+        // Return if inputs not valid
+        if (formHasErrors(errors)) {
+            return {
+                status: 422,
+                errors
+            }
+        }
+
+        // Update User entry in db
+        try {
+            await prismaClient.User.update({
+                // Set filter feilds
+                where: {
+                    id:  user.id
+                },
+                // Set update feilds
+                data: {
+                    email: formData.email.toLowerCase()
+                }
+            })
+        } catch (err) {
+            // Catch error, match error code to
+            // appropriate error message
+            switch (err.code) {
+                case "P2002":
+                    errors.email = "Email taken"
+                default:
+                    errors.server = "Unable to change information"
+            }
+        }
+
+        // Return if entry cannot be updated
+        if (formHasErrors(errors)) {
+            return {
+                status: 422,
+                errors
+            }
+        }
+
+        // Return if no errors
+        return {
+            status: 200,
+            errors
+        }
+    },
+
+    password: async ({ request, locals }) => {
+        // Variable to hold error information
+        let errors = {}
+        
+        // Get user from locals
+        const { user } = locals
+
+        // If no user
+        if (!user) {
+            return {
+                status: 403,
+                errors: {server: "User not signed in"}
+            }
+        }
+
+        // Get form data
+        const formData = Object.fromEntries(await request.formData())
+        
+        // Sanitize client input
+        if (!sanitizer.password(formData.password)) {
+            errors.password = "Invalid password"
+        }
+        if (!sanitizer.password(formData.newPassword)) {
+            errors.newPassword = "Invalid password"
+        }
+
+        // Return if inputs not valid
+        if (formHasErrors(errors)) {
+            return {
+                status: 422,
+                errors
+            }
+        }
+
+        // Get hashedPassword of User entry to be updated
+        try {
+            var response = await prismaClient.User.findUnique({
+                // Set filter feilds
+                where: {
+                    id: user.id
+                },
+                // Set return feilds
+                select: {
+                    hashedPassword: true
+                }
+            })
+        } catch (err) {
+            // Catch error, match error code to
+            // appropriate error message
+            switch (err.code) {
+                default:
+                    errors.server = "Unable to change information"
+            }
+        }
+
+        // Return if entry cannot be updated
+        if (formHasErrors(errors)) {
+            return {
+                status: 422,
+                errors
+            }
+        }
+
+        // Check if hashedPassword matches password input by client
+        try {
+            var correctPassword = await stringHasher.verify(response.hashedPassword, formData.password)
+        } catch {
+            correctPassword = false
+        }
+
+        if (!correctPassword) {
+            errors.password = "Password incorrect"
+        }
+
+        // Return if password is incorrect
+        if (formHasErrors(errors)) {
+            return {
+                status: 422,
+                errors
+            }
+        }
+
+        // Update User entry in db
+        try {
+            await prismaClient.User.update({
+                // Set filter feilds
+                where: {
+                    id: user.id
+                },
+                // Set update feilds
+                data: {
+                    // Re-hash password
+                    hashedPassword: await stringHasher.hash(formData.newPassword)
+                }
+            })
+        } catch (err) {
+            // Catch error, match error code to
+            // appropriate error message
+            switch (err.code) {
+                default:
+                    errors.server = "Unable to change information"
+            }
+        }
+
+        // Return if entry cannot be updated
+        if (formHasErrors(errors)) {
+            return {
+                status: 422,
+                errors
+            }
+        }
+
+        // Return if no errors
+        return {
+            status: 200,
+            errors
+        }
+    }
+}
---
+++ src/routes/(main)/(private)/(user)/settings/+page.svelte
@@ -0,0 +1,153 @@
+<script>
+    // Import page to get data from load functions
+    import { page } from "$app/stores"
+
+    // Import svgs
+    import Settings from "$lib/assets/svgs/Settings.svelte"
+    import Edit from "$lib/assets/svgs/Edit.svelte"
+
+    // Import components
+    import Banner from "$lib/components/Banner.svelte"
+    import AutoScroll from "$lib/components/AutoScroll.svelte"
+    import FormGroup from "$lib/components/FormGroup.svelte"
+
+    // Allows access to data returned from form action events
+    export let form
+
+    // Reactive variables to display user data
+    $: user = $page.data.user
+
+    // Define a function to shrink strings to fit in input placeholder
+    const shrinkString = (string) => {
+        if (string.length < 18) {
+            return string
+        } 
+        return (string.slice(0,8) + "..." + string.slice(-8))
+    }
+
+</script>
+
+<Banner>
+    <Settings slot="svg"/>
+    <h4>Settings</h4>
+    <AutoScroll>
+        <h6>View And Change Account Settings</h6>
+    </AutoScroll>
+</Banner>
+
+<div class="page">
+    <div class="pageContent">
+        <div class="block">
+            <FormGroup forms={[
+                {
+                    attributes: {
+                        method: "POST",
+                        action: "?/username"
+                    },
+                    inputs: [
+                        {
+                            id: "username",
+                            label: {
+                                attributes: {},
+                                text: "Username:"
+                            },
+                            attributes: {
+                                name: "username",
+                                required: true,
+                                autocomplete: "username",
+                                class: form?.errors.username ? "invalid" : "",
+                                placeholder: form?.errors.username || shrinkString(user.username)
+                            }
+                        }
+                    ]
+                },
+                {
+                    attributes: {
+                        method: "POST",
+                        action: "?/email"
+                    },
+                    inputs: [
+                        {
+                            id: "email",
+                            label: {
+                                attributes: {},
+                                text: "Email Address:"
+                            },
+                            attributes: {
+                                name: "email",
+                                required: true,
+                                autocomplete: "email",
+                                class: form?.errors.email ? "invalid" : "",
+                                placeholder: form?.errors.email || shrinkString(user.email)
+                            }
+                        }
+                    ]
+                },
+                {
+                    attributes: {
+                        method: "POST",
+                        action: "?/password"
+                    },
+                    inputs: [
+                        {
+                            id: "password",
+                            label: {
+                                attributes: {},
+                                text: "Current Password:"
+                            },                        
+                            attributes: {
+                                name: "password",
+                                required: true,
+                                autocomplete: "current-password",
+                                class: form?.errors.password ? "invalid" : "",
+                                placeholder: form?.errors.password || "Current Password"
+                            }
+                        },
+                        {
+                            id: "newPassword",
+                            label: {
+                                attributes: {},
+                                text: "Set New Password:"
+                            },
+                            attributes: {
+                                name: "newPassword",
+                                required: true,
+                                autocomplete: "new-password",
+                                class: form?.errors.newPassword ? "invalid" : "",
+                                placeholder: form?.errors.newPassword || "New Password"
+                            }
+                        },
+                    ]
+                }
+            ]}>
+                <svelte:fragment slot="title">
+                    <h5>Account Information</h5>
+                    <Edit/>
+                </svelte:fragment>
+            </FormGroup>
+        </div>
+        <div class="block">
+            <div class="blockTitle">
+                <h5>Payments</h5>
+                <Edit/>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+
+    .blockTitle {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        width: -moz-fit-content;
+        width: fit-content;
+
+        :global(svg) {
+            height: 1.25em;
+        }
+    }
+
+</style>
