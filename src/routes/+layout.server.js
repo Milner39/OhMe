4cc9cd57091b21c9ba5ef8003a88aee7a40900da
HeyItsFinnVR <finn1011@live.com>
2024-05-28 22:58:22 +0100

    Formatting and renaming variables

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,46 +1,46 @@
+// Define function to delete keys from an object
+const removeKeys = (obj, removeRule) => {
+    // Iterate over key-value pairs
+    for (const [key, value] of Object.entries(removeRule)) {
+        // If value is true, delete the key from `obj`
+        if (value === true) {
+            delete obj[key]
+
+        // If value is another object, recurse
+        } else if (typeof value === "object") {
+            removeKeys(obj[key], value)
+        }
+        // This recursion allows the object to be traversed,
+        // deleting nested keys
+    }
+}
+
+// Define keys to delete from `user` object
+const userDelete = {
+    id: true,
+    emailId: true,
+    passwordId: true,
+    email: {
+        id: true,
+        verifyCode: true
+    },
+    password: {
+        id: true,
+        hash: true,
+        resetCode: true
+    },
+    sessions: true
+}
+
 // https://kit.svelte.dev/docs/load#layout-data
 // Define load function
 export const load = async ({ locals }) => {
     // Get `user` from locals
     const user = locals.user || null
 
-    // Define keys to exclude from `user` object
-    const userExclude = {
-        id: true,
-        emailId: true,
-        passwordId: true,
-        email: {
-            id: true,
-            verifyCode: true
-        },
-        password: {
-            id: true,
-            hash: true,
-            resetCode: true
-        },
-        sessions: true
-    }
-
-    // Define function to remove excluded keys from an object
-    const removeKeys = (obj, removeRule) => {
-        // Iterate over key-value pairs
-        for (const [key, value] of Object.entries(removeRule)) {
-            // If value is true "exclude" the key from `obj`
-            if (value === true) {
-                delete obj[key]
-
-            // If value is another object, recurse
-            } else if (typeof value === "object") {
-                removeKeys(obj[key], value)
-            }
-            // This recursion allows the object to be traversed,
-            // "exluding" nested keys
-        }
-    }
-
-    if (user) removeKeys(user, userExclude)
-
-    console.log(user)
+    // Delete keys containing sensitive data
+    // to avoid client-side data leaks
+    if (user) removeKeys(user, userDelete)
 
     // Return only the `user` object
     return { user }
