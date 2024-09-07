963d7c3df03cb632582e1873dd7992fe90361aaf
HeyItsFinnVR <finn.milner@outlook.com>
2024-09-07 20:22:23 +0100

    feat: allow dev and preview scripts to serve to network

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
+// https://vitejs.dev/guide/using-plugins
+// Import SvelteKit plugin for Vite
 import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
 
-const config = defineConfig({
-	plugins: [sveltekit()]
-})
+// https://vitejs.dev/config/
+// Define Vite config
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [
+		sveltekit()
+	],
+
+	// Development settings
+	server: {
+		// Allows devices on same network to access the site
+		host: true
+	},
+
+	// Preview settings
+	preview: {
+		// Allows devices on same network to access the site
+		host: true
+	}
+}
 
 export default config
