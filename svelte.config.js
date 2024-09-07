05ae688943c174ce15421ba3955f1f79aecd39a3
HeyItsFinnVR <finn.milner@outlook.com>
2024-09-07 19:27:22 +0100

    perf: switch to correct SvelteKit adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,19 @@
-import adapter from "@sveltejs/adapter-auto"
+// https://kit.svelte.dev/docs/adapters
+// Import the correct adapter for Node servers
+import adapter from "@sveltejs/adapter-node"
 
+// https://kit.svelte.dev/docs/configuration
+// Define Svelte config
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// The directory to build the Node server
+			out: "build"
+		}),
+
+		// The directory for SvelteKit to write temp files and cache
+		outDir: ".svelte-kit",
 	}
 }
 
