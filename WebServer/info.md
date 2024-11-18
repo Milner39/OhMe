a145da28b41bc5f7ee68d8dd8619b2979ea019ed
Finn Milner <finn.milner@outlook.com>
2024-11-18 22:10:12 +0000

    style

--- WebServer/info.md
+++ WebServer/info.md
@@ -1,8 +1,10 @@
-Special consideration has to be taken into account when using Deno for a SvelteKit project.
+Special consideration has to be taken into account when using Deno for a 
+SvelteKit project.
 
-- Firstly, the SvelteKit plugin for Vite only looks for Svelte config files at `./svelte.config.js`.
-  Not `.ts`, not `.mjs`, only `.js` so using type script for the config file is out of the question.
-  Luckily JSDocs can be used for non-strict type-checking.
+- Firstly, the SvelteKit plugin for Vite only looks for Svelte config files at 
+  `./svelte.config.js`.
+  Not `.ts`, not `.mjs`, only `.js` so using type script for the config file is 
+  out of the question. Luckily JSDocs can be used for non-strict type-checking.
     
 - Secondly, the [Svelte for VS Code extension](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode)
   is terrible for Deno.
@@ -10,15 +12,16 @@ Special consideration has to be taken into account when using Deno for a SvelteK
   ```
   Error in svelte.config.js    SyntaxError: Cannot use import statement outside a module
   ```
-  This is annoying since the web app still works regardless but can be fixed with a `package.json` 
-  file with this contents in the same directory:
+  This is annoying since the web app still works regardless but can be fixed 
+  with a `package.json` file with this contents in the same directory:
   ```
   {
     "type": "module"
   }
   ```
-  The next issue is much more frustrating, since the extension language server uses Node, it only
-  supports Node dependencies, which means in order to not get a error like this:
+  The next issue is much more frustrating, since the extension language server 
+  uses Node, it only supports Node dependencies, which means in order to not get
+  an error like this:
   ```
   Error [ERR_MODULE_NOT_FOUND]: Cannot find package '@std/path' imported from ...\svelte.config.js
   ```
--- WebServer/svelte.config.js
+++ WebServer/svelte.config.js
@@ -24,14 +24,20 @@ const config = {
 	kit: {
 		adapter: adapter({
 			// The directory to build the Node server
-			out: fileURLToPath(new URL("./build", import.meta.url)),
+			out: fileURLToPath(new URL(
+				"./build",
+				import.meta.url
+			)),
 
 			// Prefix for environment variables used by the Node server
 			envPrefix: "NODE_SERVER_",
 		}),
 
 		// The directory for SvelteKit to write temp files and cache
-		outDir: fileURLToPath(new URL("./.svelte-kit", import.meta.url)),
+		outDir: fileURLToPath(new URL(
+			"./.svelte-kit",
+			import.meta.url
+		)),
 		
 		env: {
 			/*
@@ -43,20 +49,45 @@ const config = {
 		},
 		
 		files: {
-			appTemplate: fileURLToPath(new URL("./src/app.html", import.meta.url)),
-			lib: fileURLToPath(new URL("./src/lib", import.meta.url)),
-			assets: fileURLToPath(new URL("./src/lib/static", import.meta.url)),
+			appTemplate: fileURLToPath(new URL(
+				"./src/app.html",
+				import.meta.url
+			)),
+			lib: fileURLToPath(new URL(
+				"./src/lib",
+				import.meta.url
+			)),
+			assets: fileURLToPath(new URL(
+				"./src/lib/static",
+				import.meta.url
+			)),
 			hooks: {
-				client: fileURLToPath(new URL("./src/hooks.client", import.meta.url)),
-				server: fileURLToPath(new URL("./src/hooks.server", import.meta.url)),
-				universal: fileURLToPath(new URL("./src/hooks", import.meta.url))
+				client: fileURLToPath(new URL(
+					"./src/hooks.client",
+					import.meta.url
+				)),
+				server: fileURLToPath(new URL(
+					"./src/hooks.server",
+					import.meta.url
+				)),
+				universal: fileURLToPath(new URL(
+					"./src/hooks",
+					import.meta.url
+				))
 			},
-			params: fileURLToPath(new URL("./src/params", import.meta.url)),
-			routes: fileURLToPath(new URL("./src/routes", import.meta.url)),
-			serviceWorker: fileURLToPath(new URL("./src/service-worker", import.meta.url)),
+			params: fileURLToPath(new URL(
+				"./src/params",
+				import.meta.url
+			)),
+			routes: fileURLToPath(new URL(
+				"./src/routes",
+				import.meta.url
+			)),
+			serviceWorker: fileURLToPath(new URL(
+				"./src/service-worker",
+				import.meta.url
+			)),
 		},
-
-		// alias: packageJson.aliases,
 
 		typescript: {
 			config: (tsconfig) => { return tsconfig }
