c6745d31f328305c03bb6d8e864799ffd08a8354
Finn Milner <finn.milner@outlook.com>
2024-11-21 19:35:09 +0000

    docs

--- WebServer/src/lib/components/Header/Nav.svelte
+++ WebServer/src/lib/components/Header/Nav.svelte
@@ -264,8 +264,9 @@ onNavigate(() => {
 
 		<!-- 
 			svelte-ignore 
-			a11y_click_events_have_key_events,
-			a11y_no_static_element_interactions
+			a11y_click_events_have_key_events, 
+			a11y_no_static_element_interactions, 
+			a11y_interactive_supports_focus 
 		-->
 		<div class="backdrop--close-dropdown"
 			onclick={() => dropdownOpen = false}
