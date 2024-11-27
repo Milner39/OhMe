d3a02470b90aa1b18a459e3adf30cd11b32496db
Finn Milner <finn.milner@outlook.com>
2024-11-27 19:08:34 +0000

    docs

--- WebServer/src/routes/(main)/login/+page.ts
+++ WebServer/src/routes/(main)/login/+page.ts
@@ -24,7 +24,15 @@ export const load: PageLoad = ({ url }) => {
 	let mode = url.searchParams.get("mode")
 
 	// Define valid modes
-	const validModes = new Set(["login", "register", "reset-password"])
+	const validModes = new Set([
+		"login",
+		"register",
+		"reset-password"
+	])
+	/* Use `Set` because:
+		- Force unique values
+		- Faster lookup than `Array`
+	*/
 
 	// Set `mode` to default mode if is not one of the valid options
 	mode = validModes.has(mode) ? mode : "login"
