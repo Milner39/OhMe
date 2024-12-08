9cb5dfedf432fec498a43bfb80c65592807b927e
Finn Milner <finn.milner@outlook.com>
2024-12-08 22:00:59 +0000

    feat: subroutine to get if unique columns with certain values have been taken

--- Database/dbOps/user.ts
+++ Database/dbOps/user.ts
@@ -164,81 +164,170 @@ export const read = async (
 
 // #region Read per unique column
 
-export const readPerUniqueColumn = async (
+export const findTakenUniqueColumns = async (
 	filters: Partial<{
 		user: Partial<InferSelectModel<typeof tables.user>>,
 		email: Partial<InferSelectModel<typeof tables.email>>
 	}>
-) => {
+): Promise<
+	{
+		result: {
+			users: (keyof InferSelectModel<typeof tables.user> | undefined)[],
+			emails: (keyof InferSelectModel<typeof tables.email> | undefined)[]
+		},
+		error: null
+	} | {
+		result: null,
+		error: unknown
+	}
+> => {
+	try {
+		// Reduce filter to unique columns
+		const userUniqueColumnValues = filters.user ? 
+			filterUniqueColumns(filters.user, tables.user) : 
+			{} as Partial<InferSelectModel<typeof tables.user>>
 	
-	// Reduce filter to unique columns
-	const userUniqueColumnValues = filters.user ? 
-		filterUniqueColumns(filters.user, tables.user) : 
-		{} as Partial<InferSelectModel<typeof tables.user>>
-
-	const emailUniqueColumnValues = filters.email ?
-		filterUniqueColumns(filters.email, tables.email) :
-		{} as Partial<InferSelectModel<typeof tables.email>>
-
+		const emailUniqueColumnValues = filters.email ?
+			filterUniqueColumns(filters.email, tables.email) :
+			{} as Partial<InferSelectModel<typeof tables.email>>
 	
-	// Remove columns with null values since they are not unique
-	for (const column of tsObjectKeys(userUniqueColumnValues)) {
-		if (!column) continue
+		
+		// Remove columns with null values since they are not unique
+		for (const column of tsObjectKeys(userUniqueColumnValues)) {
+			if (!column) continue
+	
+			if (userUniqueColumnValues[column] === null) {
+				delete userUniqueColumnValues[column]
+			}
+		}
+	
+		for (const column of tsObjectKeys(emailUniqueColumnValues)) {
+			if (!column) continue
+	
+			if (emailUniqueColumnValues[column] === null) {
+				delete emailUniqueColumnValues[column]
+			}
+		}
+	
+	
+		// Read users based on user filters
+		const { 
+			result: users,
+			error: usersError
+		} = tsObjectKeys(userUniqueColumnValues).length > 0 ?
+			await read({
+				user: (user, { or, eq }) => or(
+					...(tsObjectEntries(userUniqueColumnValues)
+						.map((column) => {
+							if (!column) {
+								return undefined
+							}
+	
+							// @ts-ignore:
+							return eq(
+								user[column[0]], 
+								column[1]
+							)
+						})
+				))
+			}) : { 
+				result: [],
+				error: null
+			}
+		
+		if (usersError) {
+			throw new Error("Failed to read users by user filters")
+		}
+		
+		
+		// Read emails based on email filters
+		const {
+			result: usersFromEmails,
+			error: emailsError
+		} = tsObjectKeys(emailUniqueColumnValues).length > 0 ?
+			await read({
+				email: (email, { or, eq }) => or(
+					...(tsObjectEntries(emailUniqueColumnValues)
+						.map((column) => {
+							if (!column) {
+								return undefined
+							}
+	
+							// @ts-ignore:
+							return eq(
+								email[column[0]], 
+								column[1]
+							)
+						})
+				))
+			}) : { 
+				result: [],
+				error: null
+			}
 
-		if (userUniqueColumnValues[column] === null) {
-			delete userUniqueColumnValues[column]
+		if (emailsError) {
+			throw new Error("Failed to read users by email filters")
+		}
+
+		// @ts-ignore:
+		const emails = usersFromEmails.map(user => user.email) as
+			InferSelectModel<typeof tables.email>[]
+
+
+		// Find taken unique user column values
+		const takenUserColumns: 
+			(keyof typeof userUniqueColumnValues | undefined)[] = []
+
+		if (users && users.length > 0) {
+			for (const user of users ) {
+				for (const column of tsObjectKeys(userUniqueColumnValues)) {
+					if (!column) continue
+	
+					if (user[column] === userUniqueColumnValues[column]) {
+						takenUserColumns.push(column)
+					}
+				}
+			}
+		}
+
+		// Find taken unique email column values
+		const takenEmailColumns: 
+			(keyof typeof emailUniqueColumnValues | undefined)[] = []
+			
+		if (emails && emails.length > 0) {
+			for (const email of emails ) {
+				for (const column of tsObjectKeys(emailUniqueColumnValues)) {
+					if (!column) continue
+	
+					if (email[column] === emailUniqueColumnValues[column]) {
+						takenEmailColumns.push(column)
+					}
+				}
+			}
+		}
+		
+	
+		// Return results from both queries
+		return {
+			result: {
+				users: takenUserColumns,
+				emails: takenEmailColumns
+			},
+			error: null
+		}
+	} 
+
+	catch (error) {
+		return {
+			result: null,
+			error: error
 		}
 	}
-
-	for (const column of tsObjectKeys(emailUniqueColumnValues)) {
-		if (!column) continue
-
-		if (emailUniqueColumnValues[column] === null) {
-			delete emailUniqueColumnValues[column]
-		}
-	}
-
-
-	// Read users
-	const rUser = await read({
-		user: (user, { or, eq }) => or(
-			...(tsObjectEntries(userUniqueColumnValues)
-				.map((column) => {
-					if (!column) {
-						return undefined
-					}
-
-					// @ts-ignore:
-					return eq(
-						user[column[0]], 
-						column[1]
-					)
-				}
-			)
-		)),
-		email: (email, { or, eq }) => or(
-			...(tsObjectEntries(emailUniqueColumnValues)
-				.map((column) => {
-					if (!column) {
-						return undefined
-					}
-
-					// @ts-ignore:
-					return eq(
-						email[column[0]], 
-						column[1]
-					)
-				}
-			)
-		))
-	})
-
-	return rUser
 }
 
 // #endregion Read per unique column
 
-console.log(await readPerUniqueColumn({
+console.log(await findTakenUniqueColumns({
 	user: {
 		username: "Molly",
 	},
