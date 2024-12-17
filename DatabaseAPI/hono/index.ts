747b72bd809e04ec4b12f6a9ed32ba287e0c45d1
Finn Milner <finn.milner@outlook.com>
2024-12-17 16:47:59 +0000

    feat: create Hono server

---
+++ DatabaseAPI/hono/index.ts
@@ -0,0 +1,29 @@
+// #region Imports
+
+import { Hono } from "hono"
+
+// Import to get environment variables
+import { loadAllDotenvs } from "../allDotenvs.ts"
+
+// #endregion Imports
+
+
+
+// Load environment variables
+loadAllDotenvs()
+
+
+
+// Create the Hono app
+const app = new Hono()
+
+app.get("/", (c) => {
+	return c.json({ message: "Hello, world!" })
+})
+
+// Serve the app
+Deno.serve({
+		port: Number(Deno.env.get("DATABASE_API_PORT")) || 3001
+	},
+	app.fetch
+)
