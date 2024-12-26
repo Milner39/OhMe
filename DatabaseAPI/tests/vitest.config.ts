0eea60cc224e31eab33cbdbe7df5e5dca36a5f87
Finn Milner <finn.milner@outlook.com>
2024-12-26 00:35:56 +0000

    fix: Vitest not resolving path aliases

--- DatabaseAPI/tests/vitest.config.ts
+++ DatabaseAPI/tests/vitest.config.ts
@@ -1,5 +1,15 @@
 // #region Imports
 
+// Import to get file paths
+import { fileURLToPath } from "node:url"
+
+// Import to get path aliases
+import rootDenoJson from "@/deno.json" with { type: "json" }
+
+// Import to convert Deno path aliases to Vite path aliases
+import { denoAliasesToViteAliases } from "@/Utils/viteUtils.ts"
+
+
 // Import types
 import type { ViteUserConfig } from "vitest/config"
 
@@ -12,6 +22,14 @@ import type { ViteUserConfig } from "vitest/config"
 	Define Vitest config
 */
 const config = {
+
+	// Vite settings
+	cacheDir: fileURLToPath(new URL("../.vite", import.meta.url)),
+	resolve: {
+		// Path aliases
+		alias: denoAliasesToViteAliases(rootDenoJson.imports)
+	},
+
 	test: {
 		// Set env variables
 		env: {
