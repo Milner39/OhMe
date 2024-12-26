55e0f934d4fcaf0c6170dea29dce74535742a481
Finn Milner <finn.milner@outlook.com>
2024-12-26 00:34:18 +0000

    fix: prod and test db now use separate migration files

--- DatabaseAPI/drizzle/drizzle.config.ts
+++ DatabaseAPI/drizzle/drizzle.config.ts
@@ -3,6 +3,9 @@
 // Import to get db credentials
 import { getDbCredentials } from "./dbUtils.ts"
 
+// Import to get environment variables
+import env from "../env.ts"
+
 
 // Import types
 import { Config } from "drizzle-kit"
@@ -11,6 +14,9 @@ import { Config } from "drizzle-kit"
 
 
 
+// Define output directory for migration files based on environment
+const out = "drizzle/out/" + (!env.TESTING ? "prod" : "test")
+
 // Define Drizzle config
 const config = {
 	dialect: "postgresql",
@@ -18,7 +24,7 @@ const config = {
 		...getDbCredentials()
 	},
 	schema: "drizzle/src/**/schema.ts", // The files containing the schemas for each table
-	out: "drizzle/out", // The directory to store migration files
+	out: out, // The directory to store migration files
 	migrations: {
 		prefix: "timestamp", // Prefix migration files with a timestamp
 	}
