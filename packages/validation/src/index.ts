733b7bbc52d3f1509992a33dad415ee939577b7c
Finn Milner <finn.milner@outlook.com>
2025-01-02 14:42:41 +0000

    feat: add validator class

---
+++ packages/validation/src/index.ts
@@ -0,0 +1,142 @@
+// #region Imports
+
+import type { NotNull } from "#utils/src/type-utils.ts"
+
+// #endregion Imports
+
+
+
+// #region Extras
+
+const containsWhitespace = (string: string) => /\s/.test(string)
+
+// #endregion Extras
+
+
+
+// #region Validator
+
+type ValidatorResult = 
+{
+	result: true,
+	error: null
+} | {
+	result: false,
+	error: NotNull
+}
+
+/** Validator
+ * 
+ * Validates several different types of user input
+ */
+export class Validator {
+	username = (string: string): ValidatorResult => {
+		let result = false
+		const minLen = 3
+		const maxLen = 64
+
+		// Checks
+		if (typeof string !== "string") return {
+			result,
+			error: "Username must be a string"
+		}
+
+		else if (string.length < minLen) return {
+			result,
+			error: `Username must be at least ${minLen} characters`
+		}
+		else if (string.length > maxLen) return {
+			result,
+			error: `Username must be at most ${maxLen} characters`
+		}
+
+		else if (
+			containsWhitespace(string[0] + string[string.length - 1])
+		) return {
+			result,
+			error: "Username cannot start or end with a space"
+		}
+
+		// Success
+		result = true
+		return {
+			result,
+			error: null
+		}
+	}
+
+	password = (string: string): ValidatorResult => {
+		let result = false
+		const minLen = 8
+		const maxLen = 256
+
+		// Checks
+		if (typeof string !== "string") return {
+			result,
+			error: "Password must be a string"
+		}
+
+		else if (string.length < minLen) return {
+			result,
+			error: `Password must be at least ${minLen} characters`
+		}
+		else if (string.length > maxLen) return {
+			result,
+			error: `Password must be at most ${maxLen} characters`
+		}
+
+		else if (
+			containsWhitespace(string[0] + string[string.length - 1])
+		) return {
+			result,
+			error: "Password cannot start or end with a space"
+		}
+
+		// Success
+		result = true
+		return {
+			result,
+			error: null
+		}
+	}
+
+	email = (string: string): ValidatorResult => {
+		let result = false
+		const maxLen = 320
+		const emailRegex = /^[^\s@]+@[^\s@.]+(?:\.[^\s@.]+)*\.[A-Za-z]{2,}$/
+
+		// Checks
+		if (typeof string !== "string") return {
+			result,
+			error: "Email must be a string"
+		}
+
+		else if (string.length > maxLen) return {
+			result,
+			error: `Email must be at most ${maxLen} characters`
+		}
+
+		else if (containsWhitespace(string)) return {
+			result,
+			error: "Email cannot contain a space"
+		}
+		else if (!string.includes("@") || !string.includes(".")) return {
+			result,
+			error: "Email must contain '@' and '.'"
+		}
+		else if (!emailRegex.test(string)) return {
+			result,
+			error: "Email is invalid"
+		}
+
+
+		// Success
+		result = true
+		return {
+			result,
+			error: null
+		}
+	}
+}
+
+// #endregion Validator
