72c4a2812f022ab002bd78f46192a97f8545f4a5
Finn Milner <finn.milner@outlook.com>
2025-01-09 18:30:17 +0000

    fix: SvelteKit extension getting angry as usual

--- packages/utils/js-only/deno-utils.mjs
+++ packages/utils/js-only/deno-utils.mjs
@@ -1,9 +1,7 @@
-import defaultExp from "../dist/deno-utils.js"
 import { 
 	denoAliasesToAbsoluteAliases 
 } from "../dist/deno-utils.js"
 
-export default defaultExp
 export {
 	denoAliasesToAbsoluteAliases
 }
