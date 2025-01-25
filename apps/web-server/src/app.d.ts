35a6de604c00c9701a9d6ca747790ebc1cb898de
Finn Milner <finn.milner@outlook.com>
2025-01-25 16:47:53 +0000

    refactor: fix type

--- apps/web-server/src/app.d.ts
+++ apps/web-server/src/app.d.ts
@@ -1,11 +1,9 @@
-import type { SafeSelectUserSession } from "~db-api/db-orm/src/db-ops/user.ts"
+import type { SafeSelectFullUser } from "~db-api/db-orm/src/db-ops/user.ts"
 
 declare global {
 	namespace App {
 		interface Locals {
-			// Remove any if type compatibility issues are fixed
-			// Hover over type to see the problem
-			userData: SafeSelectUserSession | null | any
+			userData: SafeSelectFullUser | null
 		}
 	}
 }
