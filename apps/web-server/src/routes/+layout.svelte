4defd90599f08f08eb5557efff945c0f7a4b1690
Milner39 <91906877+Milner39@users.noreply.github.com>
2026-01-17 16:57:07 +0000

    feat: scss global stylesheet correctly loaded

--- apps/web-server/src/routes/+layout.svelte
+++ apps/web-server/src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import favicon from "$lib/assets/favicon.svg"
 
-  let { children } = $props()
+  import "$lib/styles/global.scss"  // Load global style sheet
+
+
+  const { children } = $props()
 </script>
 
 <svelte:head>
+  <!-- Load favicon -->
   <link rel="icon" href={favicon} />
 </svelte:head>
 
