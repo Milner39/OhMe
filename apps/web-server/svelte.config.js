10f7ffe5cda13ec58c56874eccd1606cd2cfdad5
Milner39 <91906877+Milner39@users.noreply.github.com>
2026-01-17 15:44:46 +0000

    chore: init svelte application

---
+++ apps/web-server/svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from "@sveltejs/adapter-node"
+import { sveltePreprocess } from "svelte-preprocess"
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: [
+    sveltePreprocess({})
+  ],
+
+  kit: {
+    adapter: adapter()
+  }
+};
+
+export default config;
