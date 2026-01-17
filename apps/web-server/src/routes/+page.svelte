5756d1f931d5bd157a3203b5015fe6c2b71dbc80
Milner39 <91906877+Milner39@users.noreply.github.com>
2026-01-17 17:27:51 +0000

    chore: try use shadcn components

--- apps/web-server/src/routes/+page.svelte
+++ apps/web-server/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/shadcn/button"
+
+</script>
+
+<Button>Click Me</Button>
